a4d54ff4573e00d7f8052ce9cc3fd298f539e52a
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2022-08-21 23:08:41 +0200

    fis retrieves bug

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,13 +10,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
+	hash_node_t *node = NULL;
 
-	hash_node_t *node = (ht->array)[index];
+	if (!ht || !key || !strcmp(key, ""))
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = (ht->array)[index];
 
 	while (node)
 	{
-		if (!strcmp(node->key, key))
+		if (!strcmp(node->key, (char *)key))
 			return (node->value);
 		node = node->next;
 	}
