b8ac91d8b4ac740b4fe92e7772a44d41fb997ee2
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2022-08-29 11:55:46 +0200

    add function declaration to the .h file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,5 +6,7 @@
 #include <unistd.h>
 
 int linear_search(int *array, size_t size, int value);
+int help_binary(int *array, int value, size_t lo, size_t hi);
 int binary_search(int *array, size_t size, int value);
+void array_print(int *array, size_t lo, size_t hi)
 #endif
